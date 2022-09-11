ed97c10f12cdf0e65a50a11a846e4f463a19a3c1
Adam Arkuszyński <adam.arkuszynski@gmail.com>
2022-09-11 09:33:34 +0200

    Initial commit Create reusable common-section module using web component with slots.

---
+++ src/js/SectionCommon.js
@@ -0,0 +1,35 @@
+  export default class SectionCommon extends HTMLElement {
+    constructor() {
+      super();
+      this.attachShadow({mode: 'open'}); 
+      this.shadowRoot.innerHTML = /*html*/`
+          <style>@import "./dist/section-common.css";</style>
+          <div class="container-common">
+            <slot name="image"></slot>
+            <div class="container-common__content">
+              <slot name="heading"></slot>
+              <slot name="paragraph"></slot>
+              <slot name="button"></slot>
+            </div>
+      `
+    }
+
+    connectedCallback() {
+      // Adding classes to the elements inserted in place of slots
+      const img = this.querySelector("img");
+      img?.classList.add("container-common__img");
+
+      const heading = this.querySelector("h1");
+      heading?.classList.add("heading");
+
+      const paragraph = this.querySelector("p");
+      paragraph?.classList.add("paragraph");
+
+      const button = this.querySelector("button");
+      console.log(button);
+      button?.classList.add("btn", "btn--primary", "container-common__button");
+    }
+
+}
+
+customElements.define('section-common', SectionCommon);
---
+++ src/js/main.js
@@ -0,0 +1,2 @@
+import SectionCommon from "./SectionCommon.js";
+
