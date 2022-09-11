055c50413c610af70f6ea49c7bb86b13c23a4d35
Adam Arkuszyński <adam.arkuszynski@gmail.com>
2022-09-11 22:07:55 +0200

    Add reset click count

--- src/js/PopupWindow.js
+++ src/js/PopupWindow.js
@@ -1,18 +1,41 @@
 export default class PopupWindow extends HTMLElement {
-  constructor(clickCount) {
+  constructor(clickCount, resetSessionCounter) {
     super();
     this.clickCount = clickCount;
+    this.resetSessionCounter = resetSessionCounter;
     this.attachShadow({mode: "open"});
+    this.render();
+    if (this.clickCount > 5) {
+      const resetButton = document.createElement("button");
+      resetButton.classList.add("btn", "btn--reset");
+      resetButton.innerText = "Reset";
+      const popupContent = this.shadowRoot.querySelector(".popup__content");
+      popupContent.appendChild(resetButton);
+      resetButton.addEventListener("click", this.handleResetClick);
+    }
+  }
+  
+  disconnectedCallback() {
+    const overlay = document.querySelector("#overlay");
+    overlay.classList.remove("active");
+  }
+
+  render = () => {
     this.shadowRoot.innerHTML = /*html*/`
     <style>@import "./dist/popup-window.css";</style>
     <div class="popup">
       <div class="popup__content">
         <div class="popup__title">Alert!</div>
-        <div class="popup-msg">You have clicked <b>${clickCount} times</b> the related button!</div>
+        <div class="popup-msg">You have clicked <b>${this.clickCount} times</b> the related button!</div>
       </div>
       <button class="close-button" aria-label="close-button">&times;</button>
     </div>
     `
+    const overlay = document.querySelector("#overlay");
+    overlay.classList.add("active");
+    const closeButon = this.shadowRoot.querySelector(".close-button");
+    closeButon.addEventListener("click", this.handleDeleteButtonClick);
+    overlay.addEventListener("click", this.handleOutsidePopupClick);
   }
 
   handleDeleteButtonClick = () => {
@@ -25,27 +48,10 @@ export default class PopupWindow extends HTMLElement {
     }
   }
 
-
-  connectedCallback() {
-    // const title = this.querySelector("h1"); // TODO
-    // const msg = this.querySelector("p"); // TODO
-    // const overlay = document.createElement("div");
-    // overlay.classList.add("active");
-    // overlay.id = "overlay"; 
-    // const body = document.querySelector("body");
-    // body.appendChild(overlay); 
-
-    const closeButon = this.shadowRoot.querySelector(".close-button");
-    const overlay = document.querySelector("#overlay");
-    overlay.classList.add("active");
-    closeButon.addEventListener("click", this.handleDeleteButtonClick);
-    overlay.addEventListener("click", this.handleOutsidePopupClick);
-  }
-
-  disconnectedCallback() {
-    const overlay = document.querySelector("#overlay");
-    overlay.classList.remove("active");
-    // overlay.remove();
+  handleResetClick = () => {
+    this.resetSessionCounter();
+    this.clickCount = 0;
+    this.render();
   }
 }
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,11 @@
 import SectionCommon from "./SectionCommon.js";
 import Popup from "./PopupWindow.js";
 
-// const clickCount = sessionStorage.getItem("clickCount");
-// if (!clickCount) {
-//   sessionStorage.setItem("clickCount", 0)
-// };
+const openPopupButtons = document.querySelectorAll("[data-target=popup]");
 
-const openPopupButtons = document.querySelectorAll("[data-popup-target]");
-
+// Adding individual click counting for popup buttons (data is stored inside session)
 openPopupButtons.forEach((button, index) => {
   button.addEventListener('click', () => {
-    // TODO Make every button store data independently
     let currentClickCount = sessionStorage.getItem("clickCount" + index);
     if (!currentClickCount) {
       currentClickCount = 0;
@@ -18,22 +13,11 @@ openPopupButtons.forEach((button, index) => {
     }
     currentClickCount ++;
     sessionStorage.setItem("clickCount" + index, currentClickCount);
-    const popup = new Popup(currentClickCount);
+    const handleResetClickCounter = () => {
+      sessionStorage.setItem("clickCount" + index, 0);
+    }
+    const popup = new Popup(currentClickCount, handleResetClickCounter);
     const body = document.querySelector("body");
   body.appendChild(popup);  
   })
 })
-
-// function handleButtonClick() {
-//   const popup = new Popup(); // pass click count
-//   const body = document.querySelector("body");
-//   body.appendChild(popup);
-// }
-
-// function handlePopupClose() {
-
-// }
-
-// document.addEventListener("click", (e) => {
-//   console.log(e.target);
-// }) 
