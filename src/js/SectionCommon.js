66315d4a41e990bb9070851287455d3a1c3c300f
Adam Arkuszyński <adam.arkuszynski@gmail.com>
2022-09-11 22:15:28 +0200

    Add dedicated render function

--- src/js/SectionCommon.js
+++ src/js/SectionCommon.js
@@ -2,6 +2,10 @@
     constructor() {
       super();
       this.attachShadow({mode: 'open'}); 
+      this.render();
+    }
+
+    render = () => {
       this.shadowRoot.innerHTML = /*html*/`
           <style>@import "./dist/section-common.css";</style>
           <div class="container-common">
@@ -12,9 +16,7 @@
               <slot name="button"></slot>
             </div>
       `
-    }
 
-    connectedCallback() { // TODO consider moving it to render function
       // Adding classes to the elements inserted in place of slots
       const img = this.querySelector("img");
       img?.classList.add("container-common__img");
@@ -28,7 +30,6 @@
       const button = this.querySelector("button");
       button?.classList.add("btn", "btn--primary", "container-common__button");
     }
-      
 }
 
 customElements.define('section-common', SectionCommon);
