d6cd9de7b954ca14fee68a1be449198e3a093df3
Adam Arkuszyński <adam.arkuszynski@gmail.com>
2022-09-13 08:59:11 +0200

    Populate section-common with conent via attributes.

--- src/js/SectionCommon.js
+++ src/js/SectionCommon.js
@@ -1,11 +1,47 @@
   export default class SectionCommon extends HTMLElement {
     constructor(img, imgAlt, heading, paragraph) {
       super();
-      this.img = img;
-      this.imgAlt = imgAlt;
-      this.heading = heading;
-      this.paragraph = paragraph;
+      this.img = this.getAttribute('img') || img;
+      this.imgAlt = this.getAttribute('img-alt') || imgAlt;
+      this.paragraph = this.getAttribute('paragraph') || paragraph;
+      this.heading = this.getAttribute('heading') || heading;
+
       this.render();
+
+      this.imgEl = this.querySelector('img');
+      this.pEl = this.querySelector('p');
+      this.headingEl = this.querySelector('h1');
+    }
+    
+    // Alter component's elements on theese attributes change
+    static get observedAttributes() {
+      return ['heading', 'img', 'img-alt', 'paragraph'];
+    }
+
+    attributeChangedCallback(name, oldVal, newVal) {
+
+      switch(name){
+        case 'heading': {
+          this.heading = newVal;
+          this.headingEl.innerHTML = this.heading;
+          break;
+        }
+        case 'img': {
+          this.img = newVal;
+          this.imgEl.setAttribute('src', newVal);
+          break;
+        }
+        case 'img-alt': {
+          this.imgAlt = newVal;
+          this.imgEl.setAttribute('alt', newVal);
+          break;
+        }
+        case 'paragraph': {
+          this.paragraph = newVal;
+          this.pEl.innerHTML = newVal;
+          break;
+        }
+      }
     }
 
     render = () => {
--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,6 @@
 import SectionCommon from "./SectionCommon.js";
 import Popup from "./PopupWindow.js";
 
-const body = document.querySelector("body");
-
-// Append common-section element to body of index.html
-const sampleImgURL = "/images/sean-o-KMn4VEeEPR8-unsplash_1_s6zmfh_c_scale,w_784.jpg";
-const sampleImgAltText = "Beach Image";
-const sampleHeading = "Lorem Ipsum";
-const sampleParagraph = `
-  Infinitely scalable, feature-rich and cloud-native data management and protection 
-  for modern and legacy infrastructures and SaaS platforms,
-  managed via a single app with no hardware required.
-`
-const sampleCommonSection = new SectionCommon(
-  sampleImgURL,
-  sampleImgAltText,
-  sampleHeading,
-  sampleParagraph
-);
-body.appendChild(sampleCommonSection);
-
 // Adding individual click-counting for popup buttons (data is stored inside session)
 const openPopupButtons = document.querySelectorAll("[data-target=popup]");
 openPopupButtons.forEach((button, index) => {
